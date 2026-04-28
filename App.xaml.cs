30f65f76bbfb21dbbcaef857705e7f9f69507e07
Paul B Colburn <paulcolburn@outlook.com>
2026-04-28 15:24:47 -0500

    added catch for unhandled exceptions

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,13 @@ namespace SpiralArt
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+#if !DEBUG
+            MessageBox.Show("An unhandled exception just occurred: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+#endif
+        }
     }
 
 }
